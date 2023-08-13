e735ebce60f3602f19af8e6176488db9ba45b19a
Eugene Cheltsov <chill.icp@gmail.com>
2023-08-13 19:56:11 +0900

    add react

---
+++ apps/playground_web/assets/js/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface AppProps {
+}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <section>
+      <h1>Welcome!!!</h1>
+    </section>
+  );
+};
+
+export default App;
